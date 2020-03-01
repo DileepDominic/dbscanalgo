3174f8af08aa1c4954a8ae0b18add1a4f82068e5
dileepdominic <dileepdominic@gmail.com>
2020-03-01 17:29:42 +0530

    Added outlier

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import pandas as pd
+
 
 from IPython.display import display
 from sklearn import metrics # for evaluations
@@ -11,8 +13,25 @@ from sklearn.preprocessing import StandardScaler # for feature scaling
 from sklearn.cluster import KMeans 
 from sklearn.cluster import DBSCAN
 
+from pylab import rcParams
+import seaborn as sb
+
+from collections import Counter
+
 # make matplotlib plot inline (Only in Ipython).
 warnings.filterwarnings('ignore')
 %matplotlib inline
 
-df = pd.read_excel(r"galaxy.xlsx")
+df = pd.read_excel(r"C:\Users\dominic\code\inputhouse\galaxy.xlsx")
+df.columns=['x-axis','y-axis']
+
+model = DBSCAN(eps=0.8,min_samples=10)
+model.fit(df)
+
+data=df.iloc[:,0:2].values
+outlier_df = pd.DataFrame(data)
+
+print(Counter(model.labels_))
+
+print(outlier_df[model.labels_ == -1])
+
